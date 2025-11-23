eaa4c2e1a122227790d0a48c0793b6760acbc34e
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-11-23 23:12:02 +0700

    feat: implement favorite list screen

--- lib/screen/favorite/favorite_screen.dart
+++ lib/screen/favorite/favorite_screen.dart
@@ -1,13 +1,59 @@
+import 'package:eateryhub/provider/favorite/favorite_provider.dart';
+import 'package:eateryhub/screen/discover/restaurant_card_widget.dart';
+import 'package:eateryhub/static/navigation_route.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class FavoriteScreen extends StatelessWidget {
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() async {
+      context.read<FavoriteProvider>().loadFavoriteRestaurants();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Favorite')),
-      body: Center(child: Text("Favorite Screen")),
+      body: Consumer<FavoriteProvider>(
+        builder: (context, value, child) {
+          final favoriteList = value.restaurantList ?? [];
+          return switch (favoriteList.isNotEmpty) {
+            true => ListView.builder(
+              itemCount: favoriteList.length,
+              itemBuilder: (context, index) {
+                final restaurant = favoriteList[index];
+                return RestaurantCardWidget(
+                  restaurant: restaurant,
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      NavigationRoute.detailRoute.name,
+                      arguments: restaurant.id,
+                    );
+                  },
+                );
+              },
+            ),
+            _ => const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("No Favorite")],
+              ),
+            ),
+          };
+        },
+      ),
     );
   }
 }
